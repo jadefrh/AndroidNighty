08d9134b71e51ed205757cab8f2886b8caa6b9f3
astronaught <jadefrh@gmail.com>
2017-06-01 22:35:02 +0200

    Formatted countdown

--- app/src/main/java/com/jadefrh/nightylogin/PatienceActivity.java
+++ app/src/main/java/com/jadefrh/nightylogin/PatienceActivity.java
@@ -1,5 +1,6 @@
 package com.jadefrh.nightylogin;
 
+import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -42,12 +43,31 @@ public class PatienceActivity extends AppCompatActivity {
                         long rem_hours = (tot_sec % SEC_PER_DAY) / SEC_PER_HOUR;
                         long rem_mins  = ((tot_sec % SEC_PER_DAY) % SEC_PER_HOUR) / SEC_PER_MIN;
                         long rem_secs  = ((tot_sec % SEC_PER_DAY) % SEC_PER_HOUR) % SEC_PER_MIN;
-                        countdown.setText(rem_hours + "hours, " + rem_mins + " minutes, " + rem_secs + " seconds remaining.");
+                        StringBuilder sb = new StringBuilder();
+                        if (rem_hours > 0) {
+                            sb.append(rem_hours);
+                            if (rem_hours == 1) {
+                                sb.append(" hour, ");
+                            } else sb.append(" hours, ");
+                        }
+                        if (rem_mins > 0 || rem_hours > 0) {
+                            sb.append(rem_mins);
+                            if (rem_mins == 1) {
+                                sb.append(" minute and ");
+                            } else sb.append(" minutes and ");
+                        }
+                        sb.append(rem_secs);
+                        if (rem_secs == 1) {
+                            sb.append(" second remaining.");
+                        } else sb.append(" seconds remaining.");
+
+                        countdown.setText(sb.toString());
 
                 }
 //
                     public void onFinish() {
-                        countdown.setText("done!");
+                        Intent i;
+                        i = new Intent(PatienceActivity.this, MoodActivity.class);
                     }
                 }.start();
             }
