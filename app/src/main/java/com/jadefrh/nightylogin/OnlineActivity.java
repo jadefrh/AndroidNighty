70892c7f1cd1205392a7d9989b2b54f1f44eb705
Raphael Zerbib <raphael.zerbib@hetic.net>
2017-06-04 00:25:24 +0200

    [page online dynamic content]

--- app/src/main/java/com/jadefrh/nightylogin/OnlineActivity.java
+++ app/src/main/java/com/jadefrh/nightylogin/OnlineActivity.java
@@ -2,11 +2,16 @@ package com.jadefrh.nightylogin;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -26,9 +31,17 @@ import static com.jadefrh.nightylogin.InitActivity.MY_PREFS_NAME;
 
 public class OnlineActivity extends AppCompatActivity {
 
+    TextView tv1, tv2;
+
+    Typeface tf1, tf2;
+
     private com.android.volley.RequestQueue queue;
     private String token;
     private Button disconnect;
+    private TextView titleConnect;
+    private TextView subtitleConnect;
+    private ImageView imageConnect;
+    private Button stateButtonConnect;
     private boolean userStatus = true;
 
     @Override
@@ -46,8 +59,24 @@ public class OnlineActivity extends AppCompatActivity {
             }
         });
 
+        titleConnect = (TextView)findViewById(R.id.titleConnect);
+        subtitleConnect = (TextView)findViewById(R.id.subtitleConnect);
+        imageConnect = (ImageView)findViewById(R.id.imageConnect);
+        stateButtonConnect = (Button)findViewById(R.id.stateButtonConnect);
+
+
+
         queue = Volley.newRequestQueue(this);
         System.out.println("alors?" + userStatus);
+
+        tv1 = (TextView) findViewById(R.id.titleConnect);
+        tv2 = (TextView) findViewById(R.id.subtitleConnect);
+
+        tf1 = Typeface.createFromAsset(getAssets(),"fonts/Gotham Rounded Bold.otf");
+        tv1.setTypeface(tf1);
+
+        tf2 = Typeface.createFromAsset(getAssets(),"fonts/SFDisplay-Regular.otf");
+        tv2.setTypeface(tf2);
     }
 
 
@@ -114,10 +143,21 @@ public class OnlineActivity extends AppCompatActivity {
 
 
     private void updateView(){
+        //si l'user est connecté, afficher ça
         if (userStatus) {
             disconnect.setText("deconnexion");
+            titleConnect.setText("Tu es connecté \uD83C\uDF89");
+            subtitleConnect.setText("Tu peux maintenant quitter l’application, tu seras notifé dès que quelqu’un qui te correspond sera connecté !");
+            imageConnect.setImageResource(R.drawable.online);
+            stateButtonConnect.setText("Je ne suis plus disponible");
+            //s'il est deconnecté afficher çaaaa
         } else {
             disconnect.setText("connexion");
+            titleConnect.setText("Tu es déconnecté \uD83D\uDE22");
+            subtitleConnect.setText("L’application ne te propose désormais plus personne. Tu peux quitter l’application avec l’assurance de ne plus rien recevoir !");
+            imageConnect.setImageResource(R.drawable.offline);
+            stateButtonConnect.setText("Se reconnecter");
+
         }
     }
 }
