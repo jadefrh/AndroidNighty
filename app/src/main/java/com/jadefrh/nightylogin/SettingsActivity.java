c9e6aa73ac0b4668dc971452f1a9442c87430838
astronaught <jadefrh@gmail.com>
2017-06-04 21:05:29 +0200

    finished settings activity

--- app/src/main/java/com/jadefrh/nightylogin/SettingsActivity.java
+++ app/src/main/java/com/jadefrh/nightylogin/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.jadefrh.nightylogin;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,6 +20,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarChangeListener;
+import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarFinalValueListener;
+import com.crystal.crystalrangeseekbar.widgets.CrystalRangeSeekbar;
 import com.facebook.login.LoginManager;
 
 import org.json.JSONException;
@@ -27,19 +31,24 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.jadefrh.nightylogin.InitActivity.MY_PREFS_NAME;
+
 public class SettingsActivity extends AppCompatActivity {
 
     private com.android.volley.RequestQueue queue;
+    private String token;
 
     private Button logout;
     private Button female;
     private Button male;
     private Button both;
     private SeekBar distanceBar;
+    private CrystalRangeSeekbar ageBar;
     private TextView distanceText;
 
-    private int distanceValue;
-    String selectedGender;
+    private int selectedDistance = 50;
+    private String selectedGender = "both";
+    private int[] selectedAge = {18, 60};
 
 
     @Override
@@ -47,10 +56,42 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        SharedPreferences prefs = this.getSharedPreferences(MY_PREFS_NAME, this.MODE_PRIVATE);
+        token = prefs.getString("nighty_access_token", null);
+
         female = (Button)findViewById(R.id.female);
         male = (Button)findViewById(R.id.male);
         both = (Button)findViewById(R.id.both);
 
+        ageBar = (CrystalRangeSeekbar) findViewById(R.id.ageBar);
+
+
+
+        ///////////////////// AGE BAR //////////////////////
+        final TextView minAge = (TextView)findViewById(R.id.minAge);
+        final TextView maxAge = (TextView)findViewById(R.id.maxAge);
+        ageBar.setMinValue(18);
+        ageBar.setMaxValue(60);
+        // set listener
+        ageBar.setOnRangeSeekbarChangeListener(new OnRangeSeekbarChangeListener() {
+            @Override
+            public void valueChanged(Number minValue, Number maxValue) {
+                minAge.setText(String.valueOf(minValue));
+                maxAge.setText(String.valueOf(maxValue));
+            }
+        });
+
+        // set final value listener
+        ageBar.setOnRangeSeekbarFinalValueListener(new OnRangeSeekbarFinalValueListener() {
+            @Override
+            public void finalValue(Number minValue, Number maxValue) {
+                selectedAge[0] = minValue.intValue();
+                selectedAge[1] = maxValue.intValue();
+                System.out.println("ageeee = " +selectedAge[0] + " - " + selectedAge[1]);
+            }
+        });
+
+        ///////////////////// DISTANCE BAR //////////////////////
         distanceText = (TextView)findViewById(R.id.distanceText);
         distanceBar = (SeekBar)findViewById(R.id.distanceBar);
         distanceText.setText("2");
@@ -69,7 +110,7 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 // TODO Auto-generated method stub
-                distanceValue = distanceBar.getProgress();
+                selectedDistance = distanceBar.getProgress();
             }
 
 
@@ -124,61 +165,52 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     private void updatePreferences() {
-        System.out.println("hihi : " + distanceValue + " and selected gender : " + selectedGender);
-//        StringRequest putRequest = new StringRequest(Request.Method.PUT, "http://nighty-develop.ivvp7jqj5r.eu-west-1.elasticbeanstalk.com/api/user/current",
-//                new Response.Listener<String>()
-//                {
-//                    @Override
-//                    public void onResponse(String response) {
-//                        // response
-//                        try {
-//                            JSONObject parentObject = new JSONObject(response);
-//                            userStatus = parentObject.getBoolean("is_online");
-//                            updateView();
-//
-//
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//                        Log.d("Response", response);
-//
-//                    }
-//                },
-//                new Response.ErrorListener()
-//                {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        // error
-//                        Log.d("Error.Response", error.getMessage());
-//                    }
-//                }
-//        ) {
-//
-//            @Override
-//            protected Map<String, String> getParams()
-//            {
-//                Map<String, String>  params = new HashMap<String, String>();
-//                if (userStatus) {
-//                    System.out.println("is online = false");
-//                    params.put("is_online", "0");
-//                } else {
-//                    System.out.println("is online = true");
-//                    params.put("is_online", "1");
-//                }
-//
-//                return params;
-//            }
-//
-//            @Override
-//            public Map<String, String> getHeaders() throws AuthFailureError {
-//                Map<String,String> params = new HashMap<String, String>();
-//                params.put("Authorization","Bearer " + token);
-//                return params;
-//            }
-//
-//        };
-//
-//        queue.add(putRequest);
+        System.out.println("selected distance : " + selectedDistance + " and selected gender : " + selectedGender);
+        System.out.println("selected ages = " + selectedAge[0] + " ------ " + selectedAge[1]);
+        StringRequest putRequest = new StringRequest(Request.Method.PUT, "http://nighty-develop.ivvp7jqj5r.eu-west-1.elasticbeanstalk.com/api/user/current",
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+
+                        Log.d("Response", response);
+
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.getMessage());
+                    }
+                }
+        ) {
+
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                    params.put("distance", String.valueOf(selectedDistance));
+                    params.put("interest", selectedGender);
+                    params.put("ages[0]", String.valueOf(selectedAge[0]));
+                    params.put("ages[1]", String.valueOf(selectedAge[1]));
+
+
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("Authorization","Bearer " + token);
+                return params;
+            }
+
+        };
+
+        queue.add(putRequest);
     }
 
 
